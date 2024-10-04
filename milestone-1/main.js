d233ea8b515cb434ef504fb415668051c983c5fe
boy136boy <162664649+boy136boy@users.noreply.github.com>
2024-10-03 22:24:45 -0700

    Add files via upload

--- milestone-1/main.js
+++ milestone-1/main.js
@@ -1,10 +1,9 @@
 var toggleButton = document.getElementById('toggle-skills');
-var skills = document.getElementById('skille');
+var skills = document.getElementById('skills'); // Match ID here
 toggleButton.addEventListener('click', function () {
     if (skills.style.display === "none" || skills.style.display === "") {
         skills.style.display = 'block';
-    }
-    else {
+    } else {
         skills.style.display = 'none';
     }
 });
